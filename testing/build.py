ac43d42b176b1af8fe4a9bbfc0fc1a79b65a4bcd
Simon Mutch <smutch@unimelb.edu.au>
2013-02-04 12:04:24 +1100

    Improve build.py to allow for cmd line build options.

--- testing/build.py
+++ testing/build.py
@@ -1,31 +1,51 @@
-from fabricate import *
+#!/usr/bin/env python
 
-comiler = 'mpicc'
+from fabricate import *
+import sys
+from glob import glob
+import optparse
+
+comiler = 'gcc'
 sources = ['test_read', 'test_propogate', 'test_walk',]
-flags = '-Wall -O2 -std=gnu99 -DDEBUG'.split()
+flags = '-Wall -O2 -std=gnu99'.split()
 # flags = '-Wall -O2'.split()
 ctags_exec = '/usr/local/bin/ctags'
 
-packages = {
-    "cn_exceptions" : {
-        "incpath" : "/Users/smutch/Code/my_packages/chuck_norris_exceptions/include",
-        "libpath" : "/Users/smutch/Code/my_packages/chuck_norris_exceptions/lib",
-    },
-    "gbpLib" : {
-        "incpath" : "/Users/smutch/3rd_party/gbpCode/myInclude",
-        "libpath" : "/Users/smutch/3rd_party/gbpCode/myLib/mpi",
-    },
-}
+extra_opts = [optparse.make_option('-o','--options', type='string',
+                                   help='Build options',
+                                   default=''),]
+
+if sys.platform == 'linux2':
+    packages = {
+        "cn_exceptions" : {
+            "incpath" : "/home/smutch/code/chuck_norris_exceptions/include",
+            "libpath" : "/home/smutch/code/chuck_norris_exceptions/lib",
+        },
+        "gbpLib" : {
+            "incpath" : "/home/smutch/3rd_party/gbpCode/myInclude",
+            "libpath" : "/home/smutch/3rd_party/gbpCode/myLib",
+        },
+    }
+else:
+    packages = {
+        "cn_exceptions" : {
+            "incpath" : "/Users/smutch/Code/my_packages/chuck_norris_exceptions/include",
+            "libpath" : "/Users/smutch/Code/my_packages/chuck_norris_exceptions/lib",
+        },
+        "gbpLib" : {
+            "incpath" : "/Users/smutch/3rd_party/gbpCode/myInclude",
+            "libpath" : "/Users/smutch/3rd_party/gbpCode/myLib",
+        },
+    }
 
 incpaths = ['-I'+packages[p]["incpath"] for p in packages.keys()]
 libpaths = ['-L'+packages[p]["libpath"] for p in packages.keys()]
 libs = ['-l'+p for p in packages.keys()]
-
+    
 def debug():
-    cflags.append('-g')
-    cflags.remove('-O2')
-    cflags.append('-O0')
-    build()
+    flags.remove('-O2')
+    flags.append('-DDEBUG -O0 -g'.split())
+    test_walk()
 
 def test_walk():
     sources = ['test_walk',]
@@ -33,14 +53,13 @@ def test_walk():
 
 def build(sources=sources):
     for source in sources:
-        run(comiler, '-o', source, 
-            'read_trees.c', source+'.c',
-            flags, incpaths, libpaths, libs)
+        run(comiler, '-o', source, 'read_trees.c', source+'.c', flags,
+            incpaths, libpaths, libs, main.options.options.split())
 
 def clean():
     autoclean()
 
 def ctags():
-    run(ctags_exec+" -R --c++-kinds=+p --fields=+iaS --extra=+q")
+    run(ctags_exec+" -R --c++-kinds=+p --fields=+iaS --extra=+q".split())
 
-main(parallel_ok=True)
+main(parallel_ok=True, extra_options=extra_opts, jobs=3)
