9f8e2851e7984689c3ccb2be63bbd69382b6d7d7
Simon Mutch <smutch@astro.swin.edu.au>
2013-06-22 12:46:42 +1000

    Add fhmodel dmvir/dt plot.

--- meraxes/utils/samtools/plots/__init__.py
+++ meraxes/utils/samtools/plots/__init__.py
@@ -1,3 +1,4 @@
 from smf import smf
 from halo_mf import halo_mf
+from fhmodel import fhmodel_dmvirdt
 from misc import *
--- meraxes/utils/samtools/plots/fhmodel.py
+++ meraxes/utils/samtools/plots/fhmodel.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python
 
-"""Plot the halo mass function"""
+"""Plots specific to the formation history model"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 __author__ = "Simon Mutch"
-__date__   = "2013/06/21"
+__date__   = "2013/06/22"
 
-def halo_mf(gals, sim_props, ax, xlim=(9,14), ylim=None, bins=20, ls='-', color=None):
+def fhmodel_dmvirdt(gals, sim_props, ax, xlim=(-4,8), ylim=None, bins=40, ls='-', color=None):
 
     # Set up the axis
     ax.set_ylabel(r"$h^3_{%.2f}\,\phi\ [Mpc^{-3}\,dex^{-1}]$" %
                   sim_props["Hubble_h"])
-    ax.set_xlabel(r"$\log_{10}(h_{%.2f}\,M_{\rm vir}/M_{\odot})$" %
-                  sim_props["Hubble_h"])
+    ax.set_xlabel(r"$\log_{10}(\frac{dM_{\rm vir}}{dt}/(M_{\odot}/yr))$")
     ax.set_yscale('log', nonposy='clip')
-    ax.set_xlim(xlim)
     ax.xaxis.set_minor_locator(plt.MaxNLocator(20))
+    if xlim!=None:
+        ax.set_xlim(xlim)
 
-    # Calculate the mass function
-    mvir = np.log10(gals['Mvir']*1.e10)
-    mvir = mvir[(mvir>=xlim[0]) & (mvir<xlim[1])]
-    n, edges = np.histogram(mvir, bins=bins, normed=False) 
+    # Calculate the distribution
+    sel = (gals['dMdt']>0)
+    dmdt = np.log10(gals[sel]['dMdt'])
+    if xlim!=None:
+        dmdt = dmdt[(dmdt>=xlim[0]) & (dmdt<xlim[1])]
+    n, edges = np.histogram(dmdt, bins=bins, normed=False) 
     bin_width = edges[1]-edges[0]
     phi = n/sim_props["Volume"]/bin_width
     bin_centers = edges[:-1]+0.5*bin_width
 
-    # Plot the result
+    # Plot
     line, = ax.plot(bin_centers, phi, ls=ls, lw=3, label="Meraxes")
     if color!=None:
         line.set_color(color)
 
     return line
-
