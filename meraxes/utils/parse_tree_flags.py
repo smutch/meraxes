6b0a492b7a9954ed3a1fb35fb2dd5bff85db39b9
Simon Mutch <smutch@unimelb.edu.au>
2014-01-21 14:13:37 +1100

    Add parse_tree_flags.py to utils

---
+++ meraxes/utils/parse_tree_flags.py
@@ -0,0 +1,32 @@
+"""Get the on switches for a merger tree flag integer.
+
+Usage: parse_tree_flags.py <flag>
+"""
+
+from docopt import docopt
+
+__author__ = "Simon Mutch"
+__date__ = "21 Jan 2014"
+
+
+class tree_flags(object):
+
+    def __init__(self):
+        self.flags = {}
+        with open("../src/tree_flags.h", "r") as fd:
+            for line in fd:
+                line = line.split()
+                if not line[0].startswith('//'):
+                    self.flags[line[1]] = int(line[2])
+
+    def parse(self, num):
+        print("Flag {:d} matches:".format(num))
+        for s, v in self.flags.iteritems():
+            if (num & v) == v:
+                print s
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    flags = tree_flags()
+    flags.parse(int(args["<flag>"]))
