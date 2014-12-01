cfd00fcaf531644a0cb572870afb81c433103cec
Simon Mutch <smutch@unimelb.edu.au>
2014-12-01 12:08:29 +1100

    Include BlackHoleMass and HotHalo mass in merger clock calc

--- meraxes/src/physics/mergers.c
+++ meraxes/src/physics/mergers.c
@@ -56,8 +56,9 @@ double calculate_merging_time(run_globals_t *run_globals, galaxy_t *sat, int sna
   coulomb = log((double)(mother->Len) / (double)(sat->Len) + 1);
 
   // Include the baryonic mass in the calculation of the dynamical friction
-  // timescale ala Guo+ 2010
-  sat_mass = sat->Mvir + sat->ColdGas + sat->StellarMass;
+  // timescale ala Guo+ 2011
+  // N.B. Hot gas should be zero in the satellite anyway for >99% of cases
+  sat_mass = sat->Mvir + sat->ColdGas + sat->StellarMass + sat->BlackHoleMass + sat->HotGas;
 
   sat_rad = (double)sqrt(
     powf(parent->Pos[0] - sat->Pos[0], 2.0) +
