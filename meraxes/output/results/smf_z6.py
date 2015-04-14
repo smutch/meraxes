164d9676ee0307104105c12af216280cfb92c799
Simon Mutch <smutch@unimelb.edu.au>
2015-04-14 15:54:19 +1000

    Add smf_z6 to results

--- meraxes/output/results/smf_z6.py
+++ meraxes/output/results/smf_z6.py
@@ -1,148 +1,89 @@
 #!/usr/bin/env python
 
-"""Plot the z=6 SMF.
+"""Plot the z=6 SMF on it's own.
 
-Usage: smf_z6.py <fname> [Hubble_h]
+Usage: smf_z6.py <fname>
 
-Arguments:
-    Hubble_h   Hubble constant [default: 0.702]
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
+from dragons import meraxes, plotutils
 from docopt import docopt
-from dragons import plotutils, meraxes, munge
+from smf import Smf, schechter
 import pandas as pd
-from astropy import log
 
 import os
 import sys
-__script_dir__ = os.path.dirname(os.path.realpath( __file__ ))
+__script_dir__ = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(__script_dir__)
-from smf_common import smf_uncert
 
 __author__ = "Simon Mutch"
-__date__   = "2014-05-01"
+__date__ = "2015-04-14"
 
-__script_dir__ = os.path.dirname(os.path.realpath( __file__ ))
+COLS = plt.rcParams["axes.color_cycle"]
+OBS_DATASETS_DIR = "/home/smutch/models/21cm_sam/meraxes/utils/obs_datasets"
+XLIM = [7, 12]
+YLIM = [-6, -1]
+HUBBLE = 0.7
 
 
-def plot(gals, simprops, ax, h):
-
-    print "Plotting z=6 SMF..."
-
-    markers = ['o', 's', 'D', 'p', 'v', '^'][::-1]
-
-    # N.B. NOTE SFR CUT TO MATCH KATSIANIS 2014 (via SMIT 2012) DATA (log(SFR)
-    # > -0.44)
-    sfr_limit = -0.265  # lower log10(sfr [Msol]) limit for galaxies
-    sfr_limit *= (0.7**2)/(h**2)
-
-    # generate the model smf
-    # sm = stellar mass
-    sm = np.log10(gals.StellarMass[(gals.StellarMass > 0)
-                                   & (np.log10(gals.Sfr) > sfr_limit)] * 1.0e10)
-
-    # n_dropped = gals.shape[0] - sm.shape[0]
-    # if n_dropped > 0:
-    #     log.warn("Dropped %d galaxies (%.1f%% of total) with stellar mass <= 0" %
-    #             (n_dropped, float(n_dropped)/gals.shape[0]*100))
-
-    smf, edges = munge.mass_function(sm, simprops["Volume"], "knuth",
-                                     return_edges=True)
-
-    # plot the model
-    l, = ax.plot(smf[:,0], np.log10(smf[:,1]), ls="--", lw=4, label=None)
-
-    # plot the total SMF (i.e. without SFR cut)
-    sm = np.log10(gals.StellarMass[gals.StellarMass > 0] * 1.0e10)
-    smf = munge.mass_function(sm, simprops["Volume"], edges,
-                              poisson_uncert=True)
-    ax.plot(smf[:,0], np.log10(smf[:,1]), ls='-', lw=4, label="Meraxes",
-            color=l.get_color())
-    # yerr = [np.log10(smf[:,1]-smf[:,2]), np.log10(smf[:,1]+smf[:,2])]
-    yerr = np.log10(smf_uncert(sm, simprops["Volume"], edges, 6.0))
-    yerr[0][np.isinf(yerr[0])] = -7
-    p = ax.fill_between(smf[:,0], yerr[0], yerr[1], color=l.get_color(),
-                    alpha=0.3)
-    r = plt.Rectangle([0,10], 0.5, 0.1, color=p.get_facecolor()[0],
-                      alpha=p.get_alpha(), label="Meraxes - convolved+Poisson")
-    ax.add_patch(r)
-
-    # read the observed smf
+def plot_obs(ax):
+    # Start with Duncan+ 2014
     obs = pd.read_table(os.path.join(__script_dir__,
-        "../../utils/obs_datasets/smf/Katsianis14_z6_smf.txt"),
+        "{:s}/smf/Duncan14_MF_z6.txt".format(OBS_DATASETS_DIR)),
         delim_whitespace=True,
-        header = None,
-        skiprows = 3,
-        names = ["sm", "log_phi", "err"])
-
-    # convert obs to same hubble value and IMF
-    obs.sm += 2.0*np.log10(0.7/h)
-    for col in ["log_phi", "err"]:
-        obs[col] -= 3.0*np.log10(0.7/h)
-
-    # plot the observations
-    ax.errorbar(obs.sm, obs.log_phi, yerr=obs.err,
-                label="Katsianis et al. 2014", ls="None",
-                lw=2, capsize=2.5, marker=markers.pop(), mec='None')
-
-    # and again...
-    obs = pd.read_table(os.path.join(__script_dir__,
-        "../../utils/obs_datasets/smf/Duncan14_MF_z6.txt"),
-        delim_whitespace=True,
-        header = None,
-        skiprows = 8,
-        names = ["sm", "phi", "merr", "perr"])
+        header=None,
+        skiprows=8,
+        names=["sm", "phi", "merr", "perr"])
     obs.merr[obs.merr >= obs.phi] = obs.phi - 1e-10
 
     # convert obs to same hubble value and IMF
-    obs.sm += 2.0*np.log10(0.702/h)
+    obs.sm += 2.0*np.log10(0.702/HUBBLE)
     obs.sm += 0.25  # IMF correction Chabrier -> Salpeter
     for col in ["phi", "merr", "perr"]:
-        obs[col] /= (0.7**3/h**3)
+        obs[col] /= (0.7**3/HUBBLE**3)
 
     # plot the observations
     ax.errorbar(obs.sm, np.log10(obs.phi),
                 yerr=[np.log10(obs.phi / (obs.phi - obs.merr)),
                       np.log10(1.0 + (obs.perr / obs.phi))],
-                label="Duncan et al. 2014", ls="None",
-                lw=2, capsize=2.5, marker=markers.pop(), mec='None')
+                label="Duncan et al. 2014", ls="None", color=COLS[1],
+                lw=2, capsize=2.5, marker='o', mec='None')
 
-    # add some text
-    ax.text(0.05,0.05, "z=6\nh={:.2f}\nSalpeter IMF\n".format(h)+
-            r"log$_{10}$(SFR)"+" > {:.2f}".format(sfr_limit)+r"M$_{\odot}$/yr",
-            horizontalalignment="left",
-            verticalalignment="bottom",
-            transform=ax.transAxes)
-
-    ax.set_xlim([7.5,11])
-    ax.set_ylim([-6,-1])
-
-    ax.set_xlabel(r"$\log_{10}(M_* / {\rm M_{\odot}})$")
-    ax.set_ylabel(r"$\log_{10}(\phi / ({\rm dex^{-1}\,Mpc^{-3}}))$")
+    # now plot Grazian+ 2015 (h=0.7, Salpeter IMF)
+    mass = np.linspace(9.5 + 2.0*np.log10(0.7/HUBBLE), XLIM[1], 30)
+    phi = np.log10(schechter(10.49, -4.16, -1.55,
+                             mass + 2.0*np.log10(HUBBLE/0.7)))\
+            + 3.0*np.log10(HUBBLE/0.7)
+    ax.plot(mass, phi, ls="--", label="Grazian et al. 2015", color=COLS[3],
+            lw=3)
 
 
 if __name__ == '__main__':
-
     args = docopt(__doc__)
-    fname = args['<fname>']
-    if args['Hubble_h'] is False:
-        h = 0.702
-    else:
-        h = float(args['Hubble_h'])
+    fname = args["<fname>"]
 
-    snap, redshift = meraxes.io.check_for_redshift(fname, 6.0, tol=0.1)
+    # set up
+    meraxes.set_little_h(HUBBLE)
+    plt.style.use(["dragons", ])
+    fig, ax = plt.subplots(1, 1)
 
-    props = ("StellarMass", "Mvir", "Type", "Sfr")
-    gals, simprops = meraxes.io.read_gals(fname, snapshot=snap, props=props,
-            sim_props=True, h=h)
-    gals = gals.view(np.recarray)
+    model = Smf(fname, 6.0)
+    model.generate(limits=XLIM)
+    # model.gen_uncert(20)
+    model.plot(ax, label="Meraxes", lw=4)
+    plot_obs(ax)
 
-    fig, ax = plt.subplots(1,1)
-    plot(gals, simprops, ax, h)
-    ax.yaxis.set_tick_params(which='both', color='w')
-    ax.legend(loc="upper right")
-    fig.tight_layout()
-    output_fname = os.path.join(os.path.dirname(fname), "plots/smf-z6.pdf")
-    plt.savefig(output_fname)
+    model.set_axlabel(ax)
+    ax.axis(XLIM + YLIM)
+    ax.legend(loc=1)
+
+    # add some text
+    ax.text(0.05, 0.05, "z=6\nh=%.1f" % HUBBLE,
+            horizontalalignment="left", verticalalignment="bottom",
+            transform=ax.transAxes)
+
+    # save
+    plt.tight_layout()
+    plt.savefig(os.path.dirname(fname)+"/plots/smf_z6.pdf")
