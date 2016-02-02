309c6a6c89f545a83064323608ec91bace5efd80
Simon Mutch <smutch@unimelb.edu.au>
2016-02-02 16:12:25 +1100

    Start doing the filtering in find_HII_bubbles

--- meraxes/src/core/tocf_find_HII_bubbles.c
+++ meraxes/src/core/tocf_find_HII_bubbles.c
@@ -15,7 +15,8 @@
  */
 
 // R in Mpc/h, M in 1e10 Msun/h 
-double RtoM(double R, int filter){
+double RtoM(double R){
+  int filter = tocf_params.HII_filter;
   double OmegaM = run_globals.params.OmegaM;
   double RhoCrit = run_globals.RhoCrit;
 
@@ -40,14 +41,11 @@ float find_HII_bubbles(float redshift)
 {
   // TODO: TAKE A VERY VERY CLOSE LOOK AT UNITS!!!!
 
-  // TODO: This should be a parameter
-  int filter = 0;
-
   float box_size = run_globals.params.BoxSize;  // Mpc/h
   int HII_dim = tocf_params.HII_dim;
   float pixel_volume = powf(box_size/(float)HII_dim, 3);  // (Mpc/h)^3
   float l_factor = 0.620350491;  // Factor relating cube length to filter radius = (4PI/3)^(-1/3)
-  float pixel_mass = RtoM(L_FACTOR*box_size/(float)HII_dim, filter);  // 1e10 Msol/h
+  float pixel_mass = RtoM(L_FACTOR*box_size/(float)HII_dim);  // 1e10 Msol/h
   float cell_length_factor = L_FACTOR;
   double ion_tvir_min = tocf_params.ion_tvir_min;
   int total_n_cells = (int)pow(HII_dim, 3);
@@ -76,20 +74,23 @@ float find_HII_bubbles(float redshift)
   // Forward fourier transform to obtain k-space fields
   // TODO: Ensure that fftwf_mpi_init has been called and fftwf_mpi_cleanup will be called
   float *deltax = run_globals.tocf_grids.deltax;
+  fftwf_complex *deltax_unfiltered = (fftwf_complex *)deltax;  // WATCH OUT!
   fftwf_complex *deltax_filtered = run_globals.tocf_grids.deltax_filtered;
-  fftwf_plan plan = fftwf_mpi_plan_dft_r2c_3d(HII_dim, HII_dim, HII_dim, deltax, (fftwf_complex *)deltax, SID_COMM_WORLD, FFTW_ESTIMATE);
+  fftwf_plan plan = fftwf_mpi_plan_dft_r2c_3d(HII_dim, HII_dim, HII_dim, deltax, deltax_unfiltered, SID_COMM_WORLD, FFTW_ESTIMATE);
   fftwf_execute(plan);
   fftwf_destroy_plan(plan);
 
   float *stars = run_globals.tocf_grids.stars;
+  fftwf_complex *stars_unfiltered = (fftwf_complex *)stars;  // WATCH OUT!
   fftwf_complex *stars_filtered = run_globals.tocf_grids.stars_filtered;
-  plan = fftwf_mpi_plan_dft_r2c_3d(HII_dim, HII_dim, HII_dim, stars, (fftwf_complex *)stars, SID_COMM_WORLD, FFTW_ESTIMATE);
+  plan = fftwf_mpi_plan_dft_r2c_3d(HII_dim, HII_dim, HII_dim, stars, stars_unfiltered, SID_COMM_WORLD, FFTW_ESTIMATE);
   fftwf_execute(plan);
   fftwf_destroy_plan(plan);
   
   float *sfr = run_globals.tocf_grids.sfr;
+  fftwf_complex *sfr_unfiltered = (fftwf_complex *)sfr;  // WATCH OUT!
   fftwf_complex *sfr_filtered = run_globals.tocf_grids.sfr_filtered;
-  plan = fftwf_mpi_plan_dft_r2c_3d(HII_dim, HII_dim, HII_dim, sfr, (fftwf_complex *)sfr, SID_COMM_WORLD, FFTW_ESTIMATE);
+  plan = fftwf_mpi_plan_dft_r2c_3d(HII_dim, HII_dim, HII_dim, sfr, sfr_unfiltered, SID_COMM_WORLD, FFTW_ESTIMATE);
   fftwf_execute(plan);
   fftwf_destroy_plan(plan);
   
@@ -99,12 +100,44 @@ float find_HII_bubbles(float redshift)
   int slab_n_complex = tocf_params.slab_n_complex[SID.My_rank];
   for (int ii=0; ii<slab_n_complex; ii++)
   {
-    deltax[ii] /= (float)total_n_cells;
-    stars[ii] /= (float)total_n_cells;
-    sfr[ii] /= (float)total_n_cells;
+    // [0] element refers to real part of complex number
+    deltax_unfiltered[ii][0] /= (float)total_n_cells;
+    stars_unfiltered[ii][0] /= (float)total_n_cells;
+    sfr_unfiltered[ii][0] /= (float)total_n_cells;
   }
 
+  // Loop through filter radii
+  float r_bubble_max = tocf_params.r_bubble_max;  // Mpc/h
+  float r_bubble_min = tocf_params.r_bubble_min;  // Mpc/h
+  float R = fmin(r_bubble_max, l_factor * box_size);  // Mpc/h
+  float delta_r_HII_factor = tocf_params.delta_r_HII_factor;
+  
+  bool flag_last_filter_step = false;
 
+  while(!flag_last_filter_step)
+  {
+    // check to see if this is our last filtering step
+    if( ((R/delta_r_HII_factor) <= (cell_length_factor*box_size/(float)HII_dim))
+        || ((R/delta_r_HII_factor) <= r_bubble_min) )
+    {
+      flag_last_filter_step = true;
+      R = cell_length_factor * box_size / (float)HII_dim;
+    }
+
+    // copy the k-space grids
+    memcpy(deltax_filtered, deltax_unfiltered, sizeof(fftwf_complex)*slab_n_complex);
+    memcpy(stars_filtered, stars_unfiltered, sizeof(fftwf_complex)*slab_n_complex);
+    memcpy(sfr_filtered, sfr_unfiltered, sizeof(fftwf_complex)*slab_n_complex);
+
+    // do the filtering unless this is the last filter step
+    if(!flag_last_filter_step)
+    {
+      HII_filter(deltax_filtered, HII_filter, R);
+      HII_filter(stars_filtered, HII_filter, R);
+      HII_filter(sfr_filtered, HII_filter, R);
+    }
+
+  }
 
 }
 
