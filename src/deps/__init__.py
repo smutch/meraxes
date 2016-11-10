8eff34b87ddf9e1e24ba0c081c5492ef9cd95682
Simon Mutch <smutch@unimelb.edu.au>
2016-11-10 06:02:18 +0000

    Add COEPP deps and CheckCCFlags to SConstruct

--- src/deps/__init__.py
+++ src/deps/__init__.py
@@ -1,8 +1,11 @@
 import sys
 import os
+import socket
 
 if os.path.exists("deps/custom.py"):
     from custom import deps
+elif 'coepp' in socket.gethostname():
+    from coepp import deps
 else:
     if sys.platform == 'darwin':
         from mac import deps
