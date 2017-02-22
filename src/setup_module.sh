796a513c5dec8214ac2fc929e048f97703489fc2
Simon Mutch <smutch@unimelb.edu.au>
2017-02-23 10:01:40 +1100

    Remove setup_module.sh

--- src/setup_module.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-module purge
-module load openmpi/x86_64/intel/1.10.2-psm gsl/x86_64/gnu/1.9  hdf5/x86_64/gnu/1.10.0-openmpi-1.10.2-psm 
-#for libp in `grep 'libp' ~/bitbucket/new_meraxes/src/deps/gstar.py| awk 'BEGIN{FS=":"}{print $2}'`; do export LD_LIBRARY_PATH=`echo $libp| tr -d "'"|tr -d ','`:$LD_LIBRARY_PATH; done
