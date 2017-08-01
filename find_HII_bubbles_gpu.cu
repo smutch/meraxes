b81382b9a81318cfc5d1fc3c1eefc7fc4712df96
Gregory Poole <gbpoole@gmail.com>
2017-08-01 11:34:28 +1000

    Full Cuda implementation of find_HII_bubbles() blocked-in.  NOT DEBUGGED. DOES NOT RUN.

--- find_HII_bubbles_gpu.cu
+++ find_HII_bubbles_gpu.cu
@@ -15,6 +15,12 @@
 #include <cufft.h>
 
 __global__
+void set_array_gpu(float *array,int n,float val){
+    int i = blockIdx.x*blockDim.x + threadIdx.x;
+    if (i < n) array[i]=val;
+}
+
+__global__
 void complex_vector_times_scalar(Complex *vector,double scalar,int n){
     int i = blockIdx.x*blockDim.x + threadIdx.x;
     if (i < n){
@@ -22,6 +28,7 @@ void complex_vector_times_scalar(Complex *vector,double scalar,int n){
         vector[i].y*=scalar;
     }
 }
+
 
 __global__
 void sanity_check_aliasing(Complex *grid,int n,float val){
@@ -50,6 +57,8 @@ __device__ float k_mag_of_index(const int dim,int index){
     return(k_mag);
 }
 
+__global__
+void filter_gpu(Complex *grid,int dim,int n,float R);
 __global__
 void filter_gpu(Complex *grid,int dim,int n,float R){
     int i = blockIdx.x*blockDim.x + threadIdx.x;
@@ -93,6 +102,116 @@ int filter_type = 0;
     }
 }
 
+__device__ int grid_index_gpu(int i, int j, int k, int dim, int type);
+__device__ int grid_index_gpu(int i, int j, int k, int dim, int type)
+{
+  int ind;
+
+  switch(type)
+  {
+    case INDEX_PADDED:
+      ind = k + (2 * (dim / 2 + 1)) * (j + dim * i);
+      break;
+    case INDEX_REAL:
+      ind = k + dim * (j + dim * i);
+      break;
+    case INDEX_COMPLEX_HERM:
+      ind = k + (dim / 2 + 1) * (j + dim * i);
+      break;
+  }
+
+  return ind;
+}
+
+__global__
+void find_HII_bubbles_gpu_main_loop(
+        float    redshift,
+        int      n,
+        int      flag_last_filter_step,
+        int      flag_ReionUVBFlag,
+        int      ReionGridDim,
+        float    R,
+        float    M,
+        float    ReionEfficiency,
+        float    inv_pixel_volume,
+        float    J_21_aux_constant,
+        double   ReionGammaHaloBias,
+        float   *xH,
+        float   *J_21,
+        float   *r_bubble,
+        float   *J_21_at_ionization,
+        float   *z_at_ionization,
+        Complex *deltax_filtered,
+        Complex *stars_filtered,
+        Complex *sfr_filtered);
+__global__
+void find_HII_bubbles_gpu_main_loop(
+        float    redshift,
+        int      n,
+        int      flag_last_filter_step,
+        int      flag_ReionUVBFlag,
+        int      ReionGridDim,
+        float    R,
+        float    M,
+        float    ReionEfficiency,
+        float    inv_pixel_volume,
+        float    J_21_aux_constant,
+        double   ReionGammaHaloBias,
+        float   *xH,
+        float   *J_21,
+        float   *r_bubble,
+        float   *J_21_at_ionization,
+        float   *z_at_ionization,
+        Complex *deltax_filtered,
+        Complex *stars_filtered,
+        Complex *sfr_filtered){
+    int index = blockIdx.x*blockDim.x + threadIdx.x;
+    if(index < n){
+        int idxs[3];
+        index2indices_FFT_k(ReionGridDim,index,idxs);
+        const int i_real   = grid_index_gpu(idxs[0], idxs[1], idxs[2], ReionGridDim, INDEX_REAL);
+        const int i_padded = grid_index_gpu(idxs[0], idxs[1], idxs[2], ReionGridDim, INDEX_PADDED);
+
+        double density_over_mean = 1.0 + (double)((float *)deltax_filtered)[i_padded];
+
+        double f_coll_stars      =  (double)((float *)stars_filtered)[i_padded] / (M * density_over_mean)
+                             * (4.0 / 3.0) * M_PI * (R*R*R)  * inv_pixel_volume;
+
+        double sfr_density       = (double)((float *)sfr_filtered)[i_padded] * inv_pixel_volume; // In internal units
+
+        float J_21_aux;
+        if (flag_ReionUVBFlag)
+          J_21_aux = (float)(sfr_density * J_21_aux_constant);
+
+        // Check if ionised!
+        if (f_coll_stars > 1.0 / ReionEfficiency)   // IONISED!!!!
+        {
+          // If it is the first crossing of the ionisation barrier for this cell (largest R), let's record J_21
+          if (xH[i_real] > REL_TOL)
+            if(flag_ReionUVBFlag)
+              J_21[i_real] = J_21_aux;
+
+          // Mark as ionised
+          xH[i_real]       = 0;
+
+          // Record radius
+          r_bubble[i_real] = (float)R;
+        }
+        // Check if this is the last filtering step.
+        // If so, assign partial ionisations to those cells which aren't fully ionised
+        else if (flag_last_filter_step && (xH[i_real] > REL_TOL))
+          xH[i_real] = (float)(1.0 - f_coll_stars * ReionEfficiency);
+
+        // Check if new ionisation
+        float *z_in = z_at_ionization;
+        if ( (xH[i_real] < REL_TOL) && (z_in[i_real] < 0) )   // New ionisation!
+        {
+          z_in[i_real] = (float)redshift;
+          if (flag_ReionUVBFlag)
+            J_21_at_ionization[i_real] = J_21_aux * (float)ReionGammaHaloBias;
+        }
+    }
+}
 
 // Presently, this is just a copy of what's in Meraxes
 void _find_HII_bubbles_gpu(
@@ -153,7 +272,6 @@ void _find_HII_bubbles_gpu(
   const int    slab_n_complex       = (int)(slabs_n_complex[mpi_rank]);
   double       cell_length_factor   = L_FACTOR;
 
-
   if (validation_output)
   {
     // prepare output file
@@ -187,24 +305,13 @@ void _find_HII_bubbles_gpu(
     H5Fclose(file_id);
   }
 
+  int threads = 256;  
+  int grid    = (slab_n_complex+255)/256;
+
   // This parameter choice is sensitive to noise on the cell size, at least for the typical
   // cell sizes in RT simulations. It probably doesn't matter for larger cell sizes.
   if ((box_size / (double)ReionGridDim) < 1.0) // Fairly arbitrary length based on 2 runs Sobacchi did
     cell_length_factor = 1.0;
-
-  // Init J_21
-  int ii;
-  if (flag_ReionUVBFlag)
-    for(ii = 0; ii < slab_n_real; ii++)
-      J_21[ii] = 0.0;
-
-  // Init xH
-  for(ii = 0; ii < slab_n_real; ii++)
-    xH[ii] = 1.0;
-
-  // Init r_bubble
-  for(ii = 0; ii < slab_n_real; ii++)
-    r_bubble[ii] = 0.0;
 
   // Forward fourier transform to obtain k-space fields
   // Initialize cuFFT
@@ -219,15 +326,31 @@ void _find_HII_bubbles_gpu(
   cufftComplex *deltax_filtered   = NULL;
   cufftComplex *stars_filtered    = NULL;
   cufftComplex *sfr_filtered      = NULL;
+  float        *xH_device                = NULL;
+  float        *J_21_device               = NULL;
+  float        *r_bubble_device          = NULL;
+  float        *z_at_ionization_device   = NULL;
+  float        *J_21_at_ionization_device = NULL;
   cudaMalloc((void**)&deltax_unfiltered,sizeof(cufftComplex)*slab_n_complex);
   cudaMalloc((void**)&stars_unfiltered, sizeof(cufftComplex)*slab_n_complex);
   cudaMalloc((void**)&sfr_unfiltered,   sizeof(cufftComplex)*slab_n_complex);
   cudaMalloc((void**)&deltax_filtered,  sizeof(cufftComplex)*slab_n_complex);
   cudaMalloc((void**)&stars_filtered,   sizeof(cufftComplex)*slab_n_complex);
   cudaMalloc((void**)&sfr_filtered,     sizeof(cufftComplex)*slab_n_complex);
+  cudaMalloc((void**)&xH_device,               sizeof(float)*slab_n_real);
+  cudaMalloc((void**)&z_at_ionization_device,  sizeof(float)*slab_n_real);
+  cudaMalloc((void**)&J_21_at_ionization_device,sizeof(float)*slab_n_real);
   cudaMemcpy(deltax,deltax_unfiltered,  sizeof(cufftComplex)*slab_n_complex,cudaMemcpyHostToDevice);
   cudaMemcpy(stars, stars_unfiltered,   sizeof(cufftComplex)*slab_n_complex,cudaMemcpyHostToDevice);
   cudaMemcpy(sfr,   sfr_unfiltered,     sizeof(cufftComplex)*slab_n_complex,cudaMemcpyHostToDevice);
+
+  // Initialize output grids
+  set_array_gpu<<<grid,threads>>>(xH_device,      slab_n_real,1.f);
+  set_array_gpu<<<grid,threads>>>(r_bubble_device,slab_n_real,0.f);
+  if (flag_ReionUVBFlag){
+     cudaMalloc((void**)&J_21_device,sizeof(float)*slab_n_real);
+     set_array_gpu<<<grid,threads>>>(J_21_device,slab_n_real,0.f);
+  }
 
   // Perform FFTs
   if (cufftExecR2C(plan,(cufftReal *)deltax_unfiltered,deltax_unfiltered) != CUFFT_SUCCESS ) {
@@ -271,16 +394,13 @@ void _find_HII_bubbles_gpu(
 
   // Remember to add the factor of VOLUME/TOT_NUM_PIXELS when converting from real space to k-space
   // Note: we will leave off factor of VOLUME, in anticipation of the inverse FFT below
-  int threads = 256;  
-  int grid    = (slab_n_complex+255)/256;
   complex_vector_times_scalar<<<grid, threads>>>(deltax_unfiltered,inv_total_n_cells,slab_n_complex);
   complex_vector_times_scalar<<<grid, threads>>>(stars_unfiltered, inv_total_n_cells,slab_n_complex);
   complex_vector_times_scalar<<<grid, threads>>>(sfr_unfiltered,   inv_total_n_cells,slab_n_complex);
 
   // Loop through filter radii
   double R                     = fmin(ReionRBubbleMax, L_FACTOR * box_size); // Mpc/h
-
-  bool  flag_last_filter_step = false;
+  bool   flag_last_filter_step = false;
 
   while(!flag_last_filter_step)
   {
@@ -298,6 +418,8 @@ void _find_HII_bubbles_gpu(
     cudaMemcpy(deltax_filtered,deltax_unfiltered,sizeof(Complex) * slab_n_complex,cudaMemcpyDeviceToDevice);
     cudaMemcpy(stars_filtered, stars_unfiltered, sizeof(Complex) * slab_n_complex,cudaMemcpyDeviceToDevice);
     cudaMemcpy(sfr_filtered,   sfr_unfiltered,   sizeof(Complex) * slab_n_complex,cudaMemcpyDeviceToDevice);
+
+    // Perform convolution
     if(!flag_last_filter_step){
        filter_gpu<<<grid,threads>>>(deltax_filtered,ReionGridDim,slab_n_complex,(float)R);
        filter_gpu<<<grid,threads>>>(stars_filtered, ReionGridDim,slab_n_complex,(float)R);
@@ -305,6 +427,7 @@ void _find_HII_bubbles_gpu(
     }
 
     // inverse fourier transform back to real space
+
     // Initialize cuFFT
     cufftPlan3d(&plan, ReionGridDim, ReionGridDim, ReionGridDim, CUFFT_C2R);
     cufftSetCompatibilityMode(plan,CUFFT_COMPATIBILITY_FFTW_ALL);
@@ -334,67 +457,39 @@ void _find_HII_bubbles_gpu(
     /*
      * Main loop through the box...
      */
-
     const double J_21_aux_constant = (1.0 + redshift) * (1.0 + redshift) / (4.0 * M_PI)
       * ReionAlphaUV * PLANCK
       * 1e21 * ReionEscapeFrac
       * R *UnitLength_in_cm * ReionNionPhotPerBary / PROTONMASS
       * UnitMass_in_g / pow(UnitLength_in_cm, 3) / UnitTime_in_s;
-
-#ifdef __NVCC__
-#else
     const double inv_pixel_volume = 1.f/pixel_volume;
-    for (ix = 0; ix < local_nix; ix++)
-      for (iy = 0; iy < ReionGridDim; iy++)
-        for (iz = 0; iz < ReionGridDim; iz++)
-        {
-          const int i_real   = grid_index(ix, iy, iz, ReionGridDim, INDEX_REAL);
-          const int i_padded = grid_index(ix, iy, iz, ReionGridDim, INDEX_PADDED);
-
-          double density_over_mean = 1.0 + (double)((float *)deltax_filtered)[i_padded];
-
-          double f_coll_stars      =  (double)((float *)stars_filtered)[i_padded] / (RtoM(R) * density_over_mean)
-                               * (4.0 / 3.0) * M_PI * pow(R,3.0)  * inv_pixel_volume;
-
-          double sfr_density       = (double)((float *)sfr_filtered)[i_padded] * inv_pixel_volume; // In internal units
-
-          float J_21_aux;
-          if (flag_ReionUVBFlag)
-            J_21_aux = (float)(sfr_density * J_21_aux_constant);
-
-          // Check if ionised!
-          if (f_coll_stars > 1.0 / ReionEfficiency)   // IONISED!!!!
-          {
-            // If it is the first crossing of the ionisation barrier for this cell (largest R), let's record J_21
-            if (xH[i_real] > REL_TOL)
-              if(flag_ReionUVBFlag)
-                J_21[i_real] = J_21_aux;
-
-            // Mark as ionised
-            xH[i_real]       = 0;
-
-            // Record radius
-            r_bubble[i_real] = (float)R;
-          }
-          // Check if this is the last filtering step.
-          // If so, assign partial ionisations to those cells which aren't fully ionised
-          else if (flag_last_filter_step && (xH[i_real] > REL_TOL))
-            xH[i_real] = (float)(1.0 - f_coll_stars * ReionEfficiency);
-
-          // Check if new ionisation
-          float *z_in = z_at_ionization;
-          if ( (xH[i_real] < REL_TOL) && (z_in[i_real] < 0) )   // New ionisation!
-          {
-            z_in[i_real] = (float)redshift;
-            if (flag_ReionUVBFlag)
-              J_21_at_ionization[i_real] = J_21_aux * (float)ReionGammaHaloBias;
-          }
-        }
-    // iz
-#endif
+    find_HII_bubbles_gpu_main_loop<<<grid,threads>>>(
+        redshift,
+        slab_n_real,
+        flag_last_filter_step,
+        flag_ReionUVBFlag,
+        ReionGridDim,
+        R,
+        RtoM(R),
+        ReionEfficiency,
+        inv_pixel_volume,
+        J_21_aux_constant,
+        ReionGammaHaloBias,
+        xH,
+        J_21,
+        r_bubble,
+        J_21_at_ionization,
+        z_at_ionization,
+        deltax_filtered,
+        stars_filtered,
+        sfr_filtered);
 
     R /= ReionDeltaRFactor;
   }
+  cudaMemcpy(xH_device,                xH,                sizeof(float) * slab_n_real,cudaMemcpyDeviceToHost);
+  cudaMemcpy(deltax_filtered,          deltax,            sizeof(float) * slab_n_real,cudaMemcpyDeviceToHost);
+  cudaMemcpy(z_at_ionization_device,   z_at_ionization,   sizeof(float) * slab_n_real,cudaMemcpyDeviceToHost);
+  cudaMemcpy(J_21_at_ionization_device,J_21_at_ionization,sizeof(float) * slab_n_real,cudaMemcpyDeviceToHost);
 
 
   // Find the volume and mass weighted neutral fractions
@@ -404,24 +499,18 @@ void _find_HII_bubbles_gpu(
   *mass_weighted_global_xH   = 0.0;
   double mass_weight         = 0.0;
 
-#ifdef __NVCC__
-#else
+  int ix,iy,iz;
   for (ix = 0; ix < local_nix; ix++)
     for (iy = 0; iy < ReionGridDim; iy++)
       for (iz = 0; iz < ReionGridDim; iz++)
       {
         const int i_real   = grid_index(ix, iy, iz, ReionGridDim, INDEX_REAL);
         const int i_padded = grid_index(ix, iy, iz, ReionGridDim, INDEX_PADDED);
-        double density_over_mean    = 1.0 + (double)((float *)deltax_filtered)[i_padded];
+        double density_over_mean    = 1.0 + (double)((float *)deltax)[i_padded];
         *volume_weighted_global_xH += (double)xH[i_real];
         *mass_weighted_global_xH   += (double)(xH[i_real]) * density_over_mean;
         mass_weight                += density_over_mean;
       }
-#endif
-
-  MPI_Allreduce(MPI_IN_PLACE, &volume_weighted_global_xH, 1, MPI_DOUBLE, MPI_SUM, mpi_comm);
-  MPI_Allreduce(MPI_IN_PLACE, &mass_weighted_global_xH, 1, MPI_DOUBLE, MPI_SUM, mpi_comm);
-  MPI_Allreduce(MPI_IN_PLACE, &mass_weight, 1, MPI_DOUBLE, MPI_SUM, mpi_comm);
 
   *volume_weighted_global_xH                        *= inv_total_n_cells;
   *mass_weighted_global_xH                          /= mass_weight;
