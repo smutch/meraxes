bfbb719136f3cf890d2837a82539297dd8a5ba00
Simon Mutch <smutch@unimelb.edu.au>
2017-10-31 15:48:16 +1100

    Bundle fftw

--- src/cmake/FindFFTW.cmake
+++ src/cmake/FindFFTW.cmake
@@ -14,33 +14,27 @@ set(FFTW_DEFINITIONS ${PC_FFTW_CFLAGS_OTHER})
 find_path(FFTW_INCLUDE_DIR fftw3.h
           PATHS ${PC_FFTW_INCLUDEDIR} ${PC_FFTW_INCLUDE_DIRS} "${FFTW_ROOT}/include")
 
-function(find_fftw_lib name dst_var)
-    # If we're asked to use static linkage, add it as a preferred library name.
-    if(FFTW_USE_STATIC)
-        list(APPEND FFTW_NAMES
-            "${CMAKE_STATIC_LIBRARY_PREFIX}lib${name}${CMAKE_STATIC_LIBRARY_SUFFIX}")
-    elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-        list(INSERT FFTW_NAMES 0
-            "${CMAKE_STATIC_LIBRARY_PREFIX}lib${name}${CMAKE_STATIC_LIBRARY_SUFFIX}")
-    endif()
+# If we're asked to use static linkage, add it as a preferred library name.
+if(FFTW_USE_STATIC)
+    list(APPEND FFTW_NAMES
+        "${CMAKE_STATIC_LIBRARY_PREFIX}libfftw3f_mpi${CMAKE_STATIC_LIBRARY_SUFFIX}")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    list(INSERT FFTW_NAMES 0
+        "${CMAKE_STATIC_LIBRARY_PREFIX}libfftw3f_mpi${CMAKE_STATIC_LIBRARY_SUFFIX}")
+endif()
 
-    list(APPEND FFTW_NAMES ${name})
+list(APPEND FFTW_NAMES fftw3f_mpi)
 
-    find_library(${dst_var} NAMES ${FFTW_NAMES}
-        PATHS "${FFTW_ROOT}/lib"
-        HINTS ${PC_FFTW_LIBDIR} ${PC_FFTW_LIBRARY_DIRS})
-endfunction()
+find_library(FFTW_LIBRARY NAMES ${FFTW_NAMES}
+    PATHS "${FFTW_ROOT}/lib"
+    HINTS ${PC_FFTW_LIBDIR} ${PC_FFTW_LIBRARY_DIRS})
 
-find_fftw_lib(fftw3f FFTW_LIBRARY)
-find_fftw_lib(fftw3f_mpi FFTW_MPI_LIBRARY)
-
-set(FFTW_LIBRARIES ${FFTW_LIBRARY} ${FFTW_MPI_LIBRARY})
+set(FFTW_LIBRARIES ${FFTW_LIBRARY})
 set(FFTW_INCLUDE_DIRS ${FFTW_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(FFTW DEFAULT_MSG
-    FFTW_LIBRARY FFTW_MPI_LIBRARY FFTW_INCLUDE_DIR)
+find_package_handle_standard_args(FFTW DEFAULT_MSG FFTW_LIBRARY FFTW_INCLUDE_DIR)
 
-mark_as_advanced(FFTW_INCLUDE_DIR FFTW_LIBRARY FFTW_MPI_LIBRARY)
+mark_as_advanced(FFTW_INCLUDE_DIR FFTW_LIBRARY)
