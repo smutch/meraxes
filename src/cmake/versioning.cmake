4ec92cddf93e4c776eca4d97ea2f5306a040065d
Simon Mutch <smutch@unimelb.edu.au>
2018-02-22 15:58:54 +1100

    Ensure gitref info is calculated whenever there are changes

---
+++ src/cmake/versioning.cmake
@@ -0,0 +1,10 @@
+find_package(Git REQUIRED)
+
+execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+    OUTPUT_VARIABLE GITREF
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND ${GIT_EXECUTABLE} --no-pager diff --no-color
+    OUTPUT_VARIABLE GITDIFF
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+string(REPLACE "\n" "\\n" GITDIFF ${GITDIFF})
+configure_file(${SOURCE_DIR}/git.h.in ${CMAKE_BINARY_DIR}/git.h ESCAPE_QUOTES @ONLY)
