dce3668ea1642c076309b07c453e8aabe5c405f4
Simon Mutch <smutch@unimelb.edu.au>
2018-05-13 21:12:37 +1000

    Set fesc even if not doing patchy reionization (for mhysa ionem constraint)

--- src/core/dracarys.c
+++ src/core/dracarys.c
@@ -302,8 +302,8 @@ void dracarys()
         // Add the ghost galaxies into the nout_gals count
         nout_gals += ghost_counter;
 
+        set_fesc(snapshot);
         if (run_globals.params.Flag_PatchyReion) {
-            set_fesc(snapshot);
 
             if (check_if_reionization_ongoing()) {
                 if (!run_globals.params.ReionUVBFlag) {
