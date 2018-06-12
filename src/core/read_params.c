b8f9a1f85de046a0d720575ad9b08e0dbe2bf692
yqiuu <yishengq@student.unimelb.edu.au>
2018-06-12 10:34:24 +1000

    Read the directory of yield tables.

--- src/core/read_params.c
+++ src/core/read_params.c
@@ -146,6 +146,11 @@ void read_parameter_file(char* fname, int mode)
             required_tag[n_param] = 1;
             params_type[n_param++] = PARAM_TYPE_STRING;
 
+            strcpy(params_tag[n_param], "YieldDir");
+            params_addr[n_param] = run_params->YieldDir;
+            required_tag[n_param] = 1;
+            params_type[n_param++] = PARAM_TYPE_STRING;
+
             strcpy(params_tag[n_param], "SSPModel");
             params_addr[n_param] = run_params->SSPModel;
             required_tag[n_param] = 1;
