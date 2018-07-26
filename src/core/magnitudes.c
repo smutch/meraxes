543ccbf44c5b5b1f8741fc628622e44e63f37847
yqiuu <yishengq@student.unimelb.edu.au>
2018-07-26 20:13:49 +1000

    Link SED library

--- src/core/magnitudes.c
+++ src/core/magnitudes.c
@@ -1,5 +1,10 @@
 #include "meraxes.h"
 
+void init_magnitudes(void) {
+    struct sed_params spectra;
+    init_templates_raw(&spectra, "./");
+}
+
 void cleanup_mags(void) {
     H5Tclose(run_globals.hdf5props.array_nmag_f_tid);
 }
