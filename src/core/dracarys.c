3bdca3e0885a0213ee6978f585376dbb61d73b00
Bradley Greig <brad.s.greig@gmail.com>
2018-08-03 15:30:09 +1000

    Added in a flag for computing the power spectrum, and where to place the function in dracarys.c

--- src/core/dracarys.c
+++ src/core/dracarys.c
@@ -324,6 +324,10 @@ void dracarys()
                 ComputeBrightnessTemperatureBox(snapshot);            
             }
 
+            if(run_globals.params.Flag_ComputePS) {
+                Compute_PS(snapshot);
+            }
+
             // if we have already created a mapping of galaxies to MPI slabs then we no
             // longer need them as they will need to be re-created for the new halo
             // positions in the next time step
--- src/core/read_params.c
+++ src/core/read_params.c
@@ -617,6 +617,11 @@ void read_parameter_file(char* fname, int mode)
             required_tag[n_param] = 1;
             params_type[n_param++] = PARAM_TYPE_INT;
 
+            strcpy(params_tag[n_param], "Flag_ComputePS");
+            params_addr[n_param] = &(run_params->Flag_ComputePS);
+            required_tag[n_param] = 1;
+	    params_type[n_param++] = PARAM_TYPE_INT;
+
             strcpy(params_tag[n_param], "Flag_IncludePecVels");
             params_addr[n_param] = &(run_params->Flag_IncludePecVels);
             required_tag[n_param] = 1;
