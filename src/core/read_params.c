d26f4f06a38d4cc693721692ccbfa422121a1786
Simon Mutch <smutch@unimelb.edu.au>
2019-05-07 21:38:49 +1000

    Do a way more flexible and useful parsing of the output snaplist

--- src/core/read_params.c
+++ src/core/read_params.c
@@ -1,5 +1,7 @@
+#include <assert.h>
 #include "meraxes.h"
 #include "parse_paramfile.h"
+
 
 static void check_problem_params(run_params_t* run_params)
 {
@@ -20,6 +22,8 @@ static void check_problem_params(run_params_t* run_params)
     }
 #endif
 }
+
+static char output_snaps_string[STRLEN] = "\0";
 
 static void store_params(entry_t entry[123],
         int n_entries,
@@ -92,6 +96,112 @@ static void store_params(entry_t entry[123],
     }
 }
 
+
+static inline int chars_to_snap(const char *string, int maxsnaps)
+{
+    int val = atoi(string);
+    if (val < 0)
+        val += maxsnaps;
+    return val;
+}
+
+
+static void parse_output_snaps(const char *string)
+{
+    if (run_globals.mpi_rank == 0) {
+        int maxsnaps = run_globals.params.SnaplistLength;
+        char sep[] = ", ";
+        char *context_outer;
+        char parse_string[STRLEN];
+
+        sprintf(parse_string, "%s", string);
+        char *p = strtok_r(parse_string, sep, &context_outer);
+
+        // count the number of requested snapshots
+        int count = 0;
+        while (p != NULL) {
+            // check to see if this token is a slice
+            if (strchr(p, ':')) {
+                char *context_inner = NULL;
+                char *sp = NULL;
+                int slice_count = 0;
+                int slice[3] = {0, maxsnaps, 1};
+
+                for(sp = strtok_r(p, ":", &context_inner); sp; sp = strtok_r(NULL, ":", &context_inner), slice_count++)
+                    slice[slice_count] = chars_to_snap(sp, maxsnaps);
+
+                count += (slice[1] - slice[0]) / slice[2] - 1;
+            }
+            count++;
+            p = strtok_r(NULL, sep, &context_outer);
+        }
+
+        // allocate the ListOutputSnaps array
+        assert(count > 0);
+        int *snaplist = calloc(count, sizeof(int));
+
+
+        // reset the token string and parse this time
+        sprintf(parse_string, "%s", string);
+        p = strtok_r(parse_string, sep, &context_outer);
+
+        count = 0;
+        while (p != NULL) {
+            // check to see if this token is a slice
+            if (strchr(p, ':')) {
+                char *context_inner = NULL;
+                char *sp = NULL;
+                int slice_count = 0;
+                int slice[3] = {0, maxsnaps, 1};
+
+                for(sp = strtok_r(p, ":", &context_inner); sp; sp = strtok_r(NULL, ":", &context_inner), slice_count++)
+                    slice[slice_count] = chars_to_snap(sp, maxsnaps);
+
+                for(int ii=slice[0]; ii<slice[1]; ii++, count++)
+                    snaplist[count] = ii;
+
+            } else {
+                snaplist[count++] = chars_to_snap(p, maxsnaps);
+            }
+            p = strtok_r(NULL, sep, &context_outer);
+        }
+
+#ifdef CALC_MAGS
+        if (*nout != NOUT) {
+            mlog_error("Number of entries in output snaplist does not match NOUT!");
+            ABORT(EXIT_FAILURE);
+        }
+#endif
+
+        // sort the list and remove any duplicates
+        qsort(snaplist, count, sizeof(int), compare_ints);
+        int jj = 0;
+        for(int ii=1; ii<count; ii++) {
+            if (snaplist[ii] != snaplist[jj])
+                snaplist[++jj] = snaplist[ii];
+        }
+        count = jj+1;
+
+        run_globals.NOutputSnaps = count;
+        run_globals.LastOutputSnap = snaplist[count-1];
+
+        run_globals.ListOutputSnaps = NULL;
+        run_globals.ListOutputSnaps = malloc(sizeof(int)*count);
+        assert(run_globals.ListOutputSnaps != NULL);
+        memcpy(run_globals.ListOutputSnaps, snaplist, sizeof(int)*count);
+    }
+
+    // broadcast the data to all other ranks
+    MPI_Bcast(&(run_globals.NOutputSnaps), 1, MPI_INT, 0, run_globals.mpi_comm);
+
+    if (run_globals.mpi_rank > 0)
+        run_globals.ListOutputSnaps = malloc(sizeof(int) * run_globals.NOutputSnaps);
+
+    MPI_Bcast(run_globals.ListOutputSnaps, run_globals.NOutputSnaps, MPI_INT, 0, run_globals.mpi_comm);
+    MPI_Bcast(&(run_globals.LastOutputSnap), 1, MPI_INT, 0, run_globals.mpi_comm);
+}
+
+
 void read_parameter_file(char* fname, int mode)
 {
     // mode = 0 : for single runs
@@ -144,6 +254,11 @@ void read_parameter_file(char* fname, int mode)
 
             strncpy(params_tag[n_param], "OutputDir", tag_length);
             params_addr[n_param] = run_params->OutputDir;
+            required_tag[n_param] = 1;
+            params_type[n_param++] = PARAM_TYPE_STRING;
+
+            strncpy(params_tag[n_param], "OutputSnapshots", tag_length);
+            params_addr[n_param] = output_snaps_string;
             required_tag[n_param] = 1;
             params_type[n_param++] = PARAM_TYPE_STRING;
 
@@ -209,11 +324,6 @@ void read_parameter_file(char* fname, int mode)
 
             strncpy(params_tag[n_param], "CatalogFilePrefix", tag_length);
             params_addr[n_param] = run_params->CatalogFilePrefix;
-            required_tag[n_param] = 1;
-            params_type[n_param++] = PARAM_TYPE_STRING;
-
-            strncpy(params_tag[n_param], "FileWithOutputSnaps", tag_length);
-            params_addr[n_param] = run_params->FileWithOutputSnaps;
             required_tag[n_param] = 1;
             params_type[n_param++] = PARAM_TYPE_STRING;
 
@@ -872,10 +982,9 @@ void read_parameter_file(char* fname, int mode)
                 ABORT(EXIT_FAILURE);
             }
 
-        if (run_globals.mpi_rank == 0) {
-            for (ii = 0; ii < n_param; ii++)
-                if (used_tag[ii] == 1) {
-                    mlog("%35s\t", MLOG_MESG, params_tag[ii]);
+        for (ii = 0; ii < n_param; ii++) {
+            if (used_tag[ii] == 1) {
+                mlog("%35s\t", MLOG_MESG, params_tag[ii]);
 
                     switch (params_type[ii]) {
                         case PARAM_TYPE_DOUBLE:
@@ -901,8 +1010,8 @@ void read_parameter_file(char* fname, int mode)
                         default:
                             mlog_error("Unknown param type.");
                             break;
-                    }
                 }
+            }
         }
 
         ii = (int)strlen(run_params->OutputDir);
@@ -912,6 +1021,9 @@ void read_parameter_file(char* fname, int mode)
 
         check_problem_params(run_params);
     } // END if(run_globals.mpi_rank==0)
+
+    // deal with the output_snaps_string which needs special parsing
+    parse_output_snaps(output_snaps_string);
 
     // If running mpi then broadcast the run parameters to all cores
     MPI_Bcast(run_params, sizeof(run_params_t), MPI_BYTE, 0, run_globals.mpi_comm);
