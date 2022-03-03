ad049ba52e4eb399fcacb309f81c57281bb0f66f
Simon Mutch <smutch@unimelb.edu.au>
2022-03-03 16:41:42 +1100

    More rejigging of project layout

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -3,9 +3,11 @@
 echo "If you haven't already, please be sure and run"
 echo "  spack compiler find && spack external find"
 echo
+echo "This bootstrap script will create a new spack environment in ../.meraxes-spack_env"
+echo
 
 while true; do
-    read -p "Do you wish to setup the spack env with all requirements? [Y/n] " yn
+    read -p "Do you wish to setup the spack env (../.meraxes-spack_env) with all requirements? [Y/n] " yn
     case $yn in
         [Yy]* ) break;;
         ''    ) break;;
@@ -14,7 +16,7 @@ while true; do
     esac
 done
 
-spack env create -d ./.spack-env spack.yaml
-eval `spack env activate --sh ./.spack-env`
+spack env create -d ../.meraxes-spack_env spack.yaml
+eval `spack env activate --sh ../.meraxes-spack_env`
 spack concretize
 spack install
