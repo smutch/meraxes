b873ec59683d472b96c8786c53859b6ffb9028e9
ManuGT <eventura@student.unimelb.edu.au>
2022-10-26 10:43:23 +1100

    Update save.c
    Fixing minor mistake

--- src/core/save.c
+++ src/core/save.c
@@ -111,10 +111,7 @@ void calc_hdf5_props()
     galaxy_output_t galout;
     int i; // dummy
 
-    h5props->n_props = 50; 
-    
-    if (run_globals.params.Flag_IncludeMetalEvo)
-      h5props->n_props +=1;
+    h5props->n_props = 51; 
 
 #ifdef CALC_MAGS
     h5props->n_props += 2;
