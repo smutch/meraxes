261bbe20eb974be7e1c463679b650d64bd6f8bda
ManuGT <eventura@student.unimelb.edu.au>
2022-11-01 16:09:17 +1100

    Adding metal bubble calculation
    Still in the regime of IRA and probably still a dumb way to compute it

--- src/physics/supernova_feedback.h
+++ src/physics/supernova_feedback.h
@@ -3,6 +3,10 @@
 
 #include "meraxes.h"
 
+#define EnergySN = 1e51 // erg, maybe you can read it from tables
+#define N_SN_Pop2 = 0.01 // number of SN x solar mass, you might add this as an input parameter
+#define N_SN_Pop3 = 0.1 // So far you don't have separation between Pop 2 and Pop 3 but soon...
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -14,6 +18,8 @@ extern "C"
                                           double m_recycled,
                                           double new_metals);
   void delayed_supernova_feedback(struct galaxy_t* gal, int snapshot);
+  void calc_metal_bubble(struct galaxy_t* gal, int snapshot);
+  double max_array(double arr[30]); 
   void contemporaneous_supernova_feedback(struct galaxy_t* gal,
                                           double* m_stars,
                                           int snapshot,
