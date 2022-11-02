1f87f0e8c09e03c560c6f2154aa6038a0f39edbb
ManuGT <eventura@student.unimelb.edu.au>
2022-11-03 10:16:08 +1100

    Update supernova_feedback.c
    Test

--- src/physics/supernova_feedback.c
+++ src/physics/supernova_feedback.c
@@ -355,9 +355,10 @@ void calc_metal_bubble(galaxy_t* gal, double* Radii[30], int snapshot) // For me
       Prefactor[*ptr] = pow(EnergySN * N_SN_Pop2 * m_stars * UnitMass_in_g / (PROTONMASS * gas_density), 0.2);
       Times[*ptr] = run_globals.LTTime[snapshot];   
     }   
-    for (int i_SF = 0; i_SF < *ptr; i_SF++){ 
-      *Radii[i_SF] = Prefactor[i_SF] * pow((run_globals.LTTime[snapshot] - Times[i_SF]), 0.4); //Mistake here!
+    //for (int i_SF = 0; i_SF < *ptr; i_SF++){ 
+    //  *Radii[i_SF] = Prefactor[i_SF] * pow((run_globals.LTTime[snapshot] - Times[i_SF]), 0.4); //Mistake here!
       //*pp[i_SF] = Prefactor[i_SF] * pow((run_globals.LTTime[snapshot] - Times[i_SF]), 0.4); //Mistake here!
+      Radii[0] = 10.0
     }
   }
   
