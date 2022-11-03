5dd7557e25b886f7b72c20d0c53e06bedcef6dd7
ManuGT <eventura@student.unimelb.edu.au>
2022-11-04 10:45:09 +1100

    Update supernova_feedback.c
    Test

--- src/physics/supernova_feedback.c
+++ src/physics/supernova_feedback.c
@@ -347,14 +347,13 @@ void calc_metal_bubble(galaxy_t* gal, int snapshot) // For metal pollution, adde
     
       //Prefactor[*count_SF] = pow(EnergySN * N_SN_Pop2 * mm_stars * UnitMass_in_g / (PROTONMASS * gas_density), 0.2);
       //Times[*count_SF] = run_globals.LTTime[snapshot];
-      //gal->Prefactor[gal->count_SF] = pow(EnergySN * N_SN_Pop2 * mm_stars * UnitMass_in_g / SOLAR_MASS / (PROTONMASS * gas_density), 0.2);
-      gal->Prefactor[0] = pow(EnergySN * N_SN_Pop2 * mm_stars * UnitMass_in_g / SOLAR_MASS / (PROTONMASS * gas_density), 0.2);
+      gal->Prefactor[gal->count_SF] = pow(EnergySN * N_SN_Pop2 * mm_stars * UnitMass_in_g / SOLAR_MASS / (PROTONMASS * gas_density), 0.2);
+      //gal->Prefactor[0] = pow(EnergySN * N_SN_Pop2 * mm_stars * UnitMass_in_g / SOLAR_MASS / (PROTONMASS * gas_density), 0.2);
       gal->Times[gal->count_SF] = run_globals.LTTime[snapshot];
     }
     if (gal->count_SF > 0) {
       for (int i_SF = 0; i_SF < gal->count_SF; i_SF++) 
         gal->Radii[i_SF] = gal->Prefactor[i_SF] * pow((run_globals.LTTime[snapshot] - gal->Times[i_SF]), 0.4); 
-        mlog_error("Tuna");
     }
   }
   
