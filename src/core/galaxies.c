37ae05380d14bfe70a45e532cb1c07cc3ddb5647
ManuGT <eventura@student.unimelb.edu.au>
2022-11-03 11:49:29 +1100

    Update galaxies.c
    Initialize new variables

--- src/core/galaxies.c
+++ src/core/galaxies.c
@@ -68,8 +68,15 @@ galaxy_t* new_galaxy(int snapshot, unsigned long halo_ID)
   gal->MergerBurstMass = 0.0;
   gal->MergerStartRadius = 0.0;
   
-  if (run_globals.params.Flag_IncludeMetalEvo) //New for Metallicity
+  if (run_globals.params.Flag_IncludeMetalEvo) { //New for Metallicity
     gal->RmetalBubble = 0.0;
+    gal->count_SF = 0;
+    for (int iii = 0; iii < 30; iii++) {
+    gal->Prefactor[iii] = (double)0.0;
+    gal->Times[iii] = (double)0.0;
+    gal->Radii[iii] = (double)0.0;
+    }
+  }
 
   for (int ii = 0; ii < 3; ii++) {
     gal->Pos[ii] = (float)-99999.9;
