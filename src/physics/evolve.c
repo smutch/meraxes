105577bacf47a076f4a86bff0428806e20e64428
ManuGT <eventura@student.unimelb.edu.au>
2022-11-04 13:59:40 +1100

    Update evolve.c

--- src/physics/evolve.c
+++ src/physics/evolve.c
@@ -61,11 +61,11 @@ int evolve_galaxies(fof_group_t* fof_group, int snapshot, int NGal, int NFof)
             if (Flag_Metals == true) 
               calc_metal_bubble(gal, snapshot);
               
-            if (gal->Radii[0] > 0)
+            if (gal->Radii[0] > 0) {
               mlog("Radius   :: %f", MLOG_MESG, gal->Radii[0]);
               mlog("Prefactor:: %f", MLOG_MESG, gal->Prefactor[0]);
               mlog("Timestep :: %f", MLOG_MESG, gal->Times[0]);
-      
+            }
             // If this is a type 2 then decrement the merger clock
             if (gal->Type == 2)
               gal->MergTime -= gal->dt;
