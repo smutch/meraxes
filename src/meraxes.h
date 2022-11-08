b012ee51ed352dd9d9ec6ca5051c4cf0c9bd35b2
ManuGT <eventura@student.unimelb.edu.au>
2022-11-08 16:55:07 +1100

    Adding a new variable to the grid
    Number of bubbles in a cell (use this to do checks)

--- src/meraxes.h
+++ src/meraxes.h
@@ -316,6 +316,7 @@ typedef struct metal_grids_t //New stuff for MetalEvo, probably you will need to
 
   int buffer_size_metals; 
   
+  int*   N_bubbles;
   float* mass_metals;
   float* mass_gas;
   float* mass_gasgal;
