5ef29dd42f53c96d08278d37b3fb69608ecbcca9
ManuGT <eventura@student.unimelb.edu.au>
2022-12-02 11:11:12 +1100

    Update evolve.c
    srand already present in init.c

--- src/physics/evolve.c
+++ src/physics/evolve.c
@@ -43,8 +43,8 @@ int evolve_galaxies(fof_group_t* fof_group, int snapshot, int NGal, int NFof, in
           if (Flag_Metals == true) { // Assign to newly formed galaxies metallicity of their cell according to a certain probability
           
             if (gal->output_index == -1) { //Not sure if Ghostflag condition is needed
-              double x=0; //Try to initialize the variable every time
-              srand(time(NULL));
+              double x;
+              //srand(time(NULL));
               x = (double)rand() / RAND_MAX;
               mlog("Random Number = %f", MLOG_MESG, x);
               
