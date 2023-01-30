779276532ef6ac2b648fad39e0eac770008a5e48
ManuGT <eventura@student.unimelb.edu.au>
2023-01-30 15:37:52 +1100

    Update virial_properties.c

--- src/core/virial_properties.c
+++ src/core/virial_properties.c
@@ -298,7 +298,7 @@ double Sigma(double redshift, double HaloMass) // Still a tiny difference
   //F.params = &(run_globals.params);
 
   gsl_integration_qag(
-    &F, 0, 5000, 1.0 / Hubble, 1.0e-8, WORKSIZE, GSL_INTEG_GAUSS21, workspace, &result, &abserr); //2500 should be infinite
+    &F, 0, 8000, 1.0 / Hubble, 1.0e-8, WORKSIZE, GSL_INTEG_GAUSS21, workspace, &result, &abserr); //2500 should be infinite
 
   gsl_integration_workspace_free(workspace);
   
@@ -331,7 +331,7 @@ double SigmaNorm(double redshift) //Need this for normalization
   //F.params = &(run_globals.params);
 
   gsl_integration_qag(
-    &F, 0, 5000, 1.0 / Hubble, 1.0e-8, WORKSIZE, GSL_INTEG_GAUSS21, workspace, &norma, &normaerr); //2500 should be infinite
+    &F, 0, 8000, 1.0 / Hubble, 1.0e-8, WORKSIZE, GSL_INTEG_GAUSS21, workspace, &norma, &normaerr); //2500 should be infinite
 
   gsl_integration_workspace_free(workspace);
   
