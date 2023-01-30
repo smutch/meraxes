5e6ba39342f1fb6057e5fab4778bf6e209506e5c
ManuGT <eventura@student.unimelb.edu.au>
2023-01-31 10:47:03 +1100

    Update virial_properties.c

--- src/core/virial_properties.c
+++ src/core/virial_properties.c
@@ -121,7 +121,8 @@ double calculate_Rvir_2(double Mvir, double redshift) //Mvir in 10^10 Msol/h
   double OmegaM = run_globals.params.OmegaM;
   double OmegaK = run_globals.params.OmegaK;
   double OmegaLambda = run_globals.params.OmegaLambda;
-  double zplus1 = redshift + 1;
+  //double zplus1 = redshift + 1;
+  double zplus = 1;
 
   hubble_of_z_sq = pow(Hubble * sqrt(OmegaM * zplus1 * zplus1 * zplus1 + OmegaK * zplus1 * zplus1 + OmegaLambda), 2);
 
@@ -144,7 +145,8 @@ double calculate_Mvir_2(double Rvir, double redshift) //Rvir in Mpc/h
   double OmegaM = run_globals.params.OmegaM;
   double OmegaK = run_globals.params.OmegaK;
   double OmegaLambda = run_globals.params.OmegaLambda;
-  double zplus1 = redshift + 1;
+  //double zplus1 = redshift + 1;
+  double zplus1 = 1;
   
   hubble_of_z_sq = pow(Hubble * sqrt(OmegaM * zplus1 * zplus1 * zplus1 + OmegaK * zplus1 * zplus1 + OmegaLambda), 2);
 
