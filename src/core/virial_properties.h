7772f40f093c876d1a7eb3306155f1db0f350061
ManuGT <eventura@student.unimelb.edu.au>
2023-01-30 16:25:42 +1100

    Trying to make things more organized

--- src/core/virial_properties.h
+++ src/core/virial_properties.h
@@ -10,9 +10,11 @@ extern "C"
 
   double Tvir_to_Mvir(double T, double z);
   double calculate_Mvir(double Mvir, int len);
+  double calculate_Mvir_2(double Rvir, double redshift);
   double hubble_at_snapshot(int snapshot);
   double hubble_time(int snapshot);
   double calculate_Rvir(double Mvir, int snapshot);
+  double calculate_Rvir_2(double Mvir, double redshift);
   double calculate_Vvir(double Mvir, double Rvir);
   double calculate_spin_param(halo_t* halo);
   double calculate_gasMass(int snapshot, double length);
