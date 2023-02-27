acd0b20dbebb32e54c30026adbdd24e90c776a46
ManuGT <eventura@student.unimelb.edu.au>
2023-02-28 10:24:32 +1100

    Restore

--- src/core/dracarys.c
+++ src/core/dracarys.c
@@ -329,17 +329,17 @@ void dracarys()
     double Pk_values[45];
     mlog("Norma %f", MLOG_MESG, Sigma(0, calculate_Mvir_2(8.0,0)));
     for (int iii = 0; iii < 45; iii++) {
-      Mvir_values[iii] = calculate_Mvir_2(array_values[iii] / hh, 0);
+      Mvir_values[iii] = calculate_Mvir_2(array_values[iii], 0);
       //Rvir_values[iii] = calculate_Rvir_2(Mvir_values[iii], 10);
       Sigma_values[iii] = Sigma(10, Mvir_values[iii]) * Growth_Factor(10) / Growth_Factor(0);
-      TF_values[iii] = Transfer_function(array_values[iii] / hh);
-      Pk_values[iii] = PowerSpectrum(10, array_values[iii] / hh);
+      TF_values[iii] = Transfer_function(array_values[iii]);
+      Pk_values[iii] = PowerSpectrum(10, array_values[iii]);
       //CF_values[iii] = TwoPointCF(array_values[iii], boost_R0);
       //CF_values[iii] = TwoPointCF_2(10.0, 1.15e-2, array_values[iii]);
       //CF_values[iii] = TwoPointCF_2(10.0, 1.15e-2, Rvir_values[iii]);
       //mlog("CFvalue %f Radius %f", MLOG_MESG, CF_values[iii], array_values[iii] / hh);
       //mlog("CFvalue %f Radius %f", MLOG_MESG, CF_values[iii], Rvir_values[iii]);
-      mlog("Sigma (z = 10) %f, Pk %f, Tk %f, R %f Mvir %f", MLOG_MESG, Sigma_values[iii], Pk_values[iii], TF_values[iii], array_values[iii] / hh, Mvir_values[iii] * 1e10);
+      mlog("Sigma (z = 10) %f, Pk %f, Tk %f, R %f Mvir %f", MLOG_MESG, Sigma_values[iii], Pk_values[iii], TF_values[iii], array_values[iii] / hh, Mvir_values[iii] * 1e10 / hh);
       //mlog("Sigma (z = 10) %f", MLOG_MESG, Sigma_values[iii]);
       //mlog("TF (z = 10), Tf %f", MLOG_MESG, TF_values[iii]);
       }
