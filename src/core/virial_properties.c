682bf59bc485d8f24f70b532e9c81ee1a75114ef
ManuGT <eventura@student.unimelb.edu.au>
2023-02-28 03:14:33 +1100

    Update virial_properties.c

--- src/core/virial_properties.c
+++ src/core/virial_properties.c
@@ -365,7 +365,7 @@ double integrand_S2(double k, void* params)
   double little_h = run_globals.params.Hubble_h;
   
   //double Radius = calculate_Rvir_2(p->HaloMass, p->redshift); //Compute Rvir in cMpc/h
-  double Radius = calculate_Rvir_2(p->HaloMass, 0);
+  double Radius = calculate_Rvir_2(p->HaloMass, p->redshift);
   
   double PS = PowerSpectrum(p->redshift, k);
   
