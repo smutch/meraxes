676b52ed8d109cd87dd0ea95a796566bd0045b6a
ManuGT <eventura@student.unimelb.edu.au>
2023-04-12 16:53:24 +1000

    Test

--- src/core/PopIII.h
+++ src/core/PopIII.h
@@ -17,7 +17,7 @@ extern "C"
 #endif
   
   void initialize_time_interp_arrays();
-  double CCSN_PopIII_Fraction(int snapshot); 
+  double CCSN_PopIII_Fraction(int Snapshot, int last_snap); 
   float interp_mass(float lifetime);
   double IMFnorm(double MminIMF, double MmaxIMF);
   double get_StellarAge(double StarMass);
