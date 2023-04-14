65eac7bf230f0d04b77527f99f4051077ddd0edf
ManuGT <eventura@student.unimelb.edu.au>
2023-04-14 15:26:11 +1000

    Strange things

--- src/core/PopIII.h
+++ src/core/PopIII.h
@@ -17,7 +17,7 @@ extern "C"
 #endif
   
   void initialize_time_interp_arrays();
-  double CCSN_PopIII_Fraction(int Snapshot, int last_snap); 
+  double CCSN_PopIII_Fraction(int i_burst, int curr_snap); 
   double interp_mass(double lifetime);
   double IMFnorm(double MminIMF, double MmaxIMF);
   double get_StellarAge(double StarMass);
