0503fe853ea3403079afdebc4322fe94ef05cea3
ManuGT <eventura@student.unimelb.edu.au>
2023-04-14 14:37:56 +1000

    Chasing integration error

--- src/physics/star_formation.c
+++ src/physics/star_formation.c
@@ -194,7 +194,9 @@ void insitu_star_formation(galaxy_t* gal, int snapshot)
       m_stars = gal->ColdGas;
     // calculate the total supernova feedback which would occur if this star
     // formation happened continuously and evenly throughout the snapshot
+    mlog_error("C1");
     contemporaneous_supernova_feedback(gal, &m_stars, snapshot, &m_reheat, &m_eject, &m_recycled, &new_metals);
+    mlog_error("C2");
     // update the baryonic reservoirs (note that the order we do this in will change the result!)
     update_reservoirs_from_sf(gal, m_stars, snapshot, INSITU);
     update_reservoirs_from_sn_feedback(gal, m_reheat, m_eject, m_recycled, new_metals);
