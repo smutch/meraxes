0f81bd8559544a3b0c0a60655e94220a3c004240
Yuxiang <Yuxiang.L.Qin@gmail.com>
2023-07-17 17:58:50 +1000

    mpi error for rhocrit

--- src/core/init.c
+++ src/core/init.c
@@ -143,6 +143,7 @@ static void read_snap_list()
     run_globals.AA = malloc(sizeof(double) * run_globals.params.SnaplistLength);
     run_globals.ZZ = malloc(sizeof(double) * run_globals.params.SnaplistLength);
     run_globals.LTTime = malloc(sizeof(double) * run_globals.params.SnaplistLength);
+    run_globals.rhocrit = malloc(sizeof(double) * run_globals.params.SnaplistLength);
   }
   MPI_Bcast(run_globals.AA, run_globals.params.SnaplistLength, MPI_DOUBLE, 0, run_globals.mpi_comm);
 }
