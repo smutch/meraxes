c040d40c2eaf9bb526e612507462e48ef29a5a53
ManuGT <eventura@student.unimelb.edu.au>
2023-07-18 11:35:34 +1000

    Update star_formation.c
    Typo

--- src/physics/star_formation.c
+++ src/physics/star_formation.c
@@ -41,6 +41,7 @@ void update_reservoirs_from_sf(galaxy_t* gal, double new_stars, int snapshot, SF
   if (new_stars > 0) {
     double metallicity;
     bool Flag_IRA = (bool)(run_globals.params.physics.Flag_IRA);
+    bool Flag_Metals = (bool)(run_globals.params.Flag_IncludeMetalEvo);
 
     // instantaneous recycling approximation of stellar mass
     metallicity = calc_metallicity(gal->ColdGas, gal->MetalsColdGas);
